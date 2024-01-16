0a712c27fdf2272cabfaf583865c5039226a1c99
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2024-01-16 16:48:20 +0100

    Models

--- kanban/models.py
+++ kanban/models.py
@@ -4,26 +4,29 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Bereich(models.Model):
     title = models.CharField(("Titel"), max_length=100)
-    beschreibung = models.TextField(("Beschreibung"), blank=True, null=True)
+    description = models.TextField(("Beschreibung"), blank=True, null=True)
     
     class Meta:
         verbose_name = ("Bereich")
         verbose_name_plural = ("Bereiche")
 
     def __str__(self):
-        return self.name
+        return self.title
 
     def get_absolute_url(self):
         return reverse("Bereich_detail", kwargs={"pk": self.pk})
 
 class Project(models.Model):
     title = models.CharField(("Titel"), max_length=100)
+    privat = models.BooleanField(("Privat"))
+    field = models.ForeignKey(Bereich, verbose_name=("Bereich"), on_delete=models.CASCADE)
+    prio = models.IntegerField(("Priorität"))
     class Meta:
         verbose_name = ("Projekt")
         verbose_name_plural = ("Projekte")
 
     def __str__(self):
-        return self.name
+        return self.title
 
     def get_absolute_url(self):
         return reverse("Project_detail", kwargs={"pk": self.pk})
@@ -31,14 +34,17 @@ class Project(models.Model):
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
-
+    title = models.CharField(("Titel"), max_length=50)
+    description = models.TextField(("Beschreibung"), null = True, blank = True)
+    project = models.ForeignKey(Project, verbose_name=("Projekt"), on_delete=models.CASCADE)
+    cyclical = models.BooleanField(("Zyklisch"), default=False)
+    complete = models.BooleanField(("Fertig"), default=False)
     class Meta:
         verbose_name = ("Aufgabe")
         verbose_name_plural = ("Aufgaben")
 
     def __str__(self):
-        return self.name
+        return f"{self.title}/{self.project}/{self.user}"
 
     def get_absolute_url(self):
         return reverse("Task_detail", kwargs={"pk": self.pk})
